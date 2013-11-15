cb715661ef28cd32841173f264cf5c9381900cbe
Stephen Mathieson <smath23@gmail.com>
2013-11-15 13:28:10 -0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,20 @@
+
+# focus-trap
+
+  Trap focus within an element
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install stephenmathieson/focus-trap
+
+## API
+
+### `focusTrap(container)`
+
+  Keep focus within `container`.  Suitable for modals, etc.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,42 @@
+
+var focusable = require('focusable-elements');
+var ev = require('event');
+var normalize = require('normalize');
+
+/**
+ * Expose `trapFocus`
+ */
+
+module.exports = focusTrap;
+
+/**
+ * Trap focus within `container`
+ *
+ * @api public
+ * @param {HTMLElement} container
+ * @return {Function} keyboard listener
+ */
+
+function focusTrap(container) {
+  var elements = focusable(container);
+  var first = elements[0];
+  var last = elements[elements.length - 1];
+
+  return ev.bind(container, 'keydown', function (keyboardEvent) {
+    keyboardEvent = normalize(keyboardEvent);
+
+    if (9 !== keyboardEvent.which) return;
+
+    if (keyboardEvent.shiftKey) {
+      if (first === keyboardEvent.target) {
+        keyboardEvent.preventDefault();
+        last.focus();
+      }
+    } else {
+      if (last === keyboardEvent.target) {
+        keyboardEvent.preventDefault();
+        first.focus();
+      }
+    }
+  });
+}
